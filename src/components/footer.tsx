5ba9921403bb2807be185a113475387bac876546
autumn_dima <ozerland@gmail.com>
2025-01-07 05:05:53 +0200

    install ui libs; create header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,27 @@
+import { navigation } from '@/config';
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Footer() {
-  return <header>Hello footer</header>;
+  return (
+    <footer className="bg-green-950 text-white py-10">
+      <div className="flex flex-col mx-auto gap-20  max-w-2xl lg:max-w-7xl px-5  justify-between ">
+        <div className="flex justify-between">
+          <Link href="/" className="-ml-3.5 p-1.5">
+            <span className="sr-only">Store</span>
+            <Image src="/logo.svg" alt="Next.js logo" width={180} height={38} priority />
+          </Link>
+          <div className="flex flex-col gap-3 self-end">
+            {navigation.map(({ href, title }) => (
+              <Link key={href} href={href} className="text-sm/2 font-semibold text-white hover:text-green-400">
+                {title}
+              </Link>
+            ))}
+          </div>
+        </div>
+
+        <span className="self-center">Copyright © {new Date().getFullYear()} TechCart. All rights reserved.</span>
+      </div>
+    </footer>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,77 @@
+'use client';
+
+import { useState } from 'react';
+import { Dialog, DialogPanel, PopoverGroup } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
+import Link from 'next/link';
+import { navigation } from '@/config';
+
 export default function Header() {
-  return <footer>Hello header</footer>;
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="bg-white">
+      <nav aria-label="Global" className="mx-auto flex max-w-2xl lg:max-w-7xl px-5 items-center justify-between">
+        <div className="flex z-10">
+          <Link href="/" className="-ml-3.5 p-1.5">
+            <span className="sr-only">Store</span>
+            <Image src="/logo.svg" alt="Next.js logo" width={180} height={38} priority />
+          </Link>
+        </div>
+        <div className="flex z-10 lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <PopoverGroup className="hidden z-10 lg:flex lg:flex-1 lg:justify-end lg:gap-x-12">
+          {navigation.map(({ href, title }) => (
+            <Link key={href} href={href} className="text-sm/2 font-semibold text-gray-900 hover:text-green-400">
+              {title}
+            </Link>
+          ))}
+        </PopoverGroup>
+      </nav>
+      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+        <div className="fixed inset-0 z-20" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-ml-3.5 p-1.5">
+              <span className="sr-only">Store</span>
+              <Image src="/logo.svg" alt="Next.js logo" width={180} height={38} priority />
+            </a>
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {navigation.map(({ href, title }) => (
+                  <Link
+                    key={href}
+                    href={href}
+                    onClick={() => setMobileMenuOpen(false)}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    {title}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
 }
