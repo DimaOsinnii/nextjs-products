7d7610925af4b0f264ba276174312bd39682c8d6
autumn_dima <ozerland@gmail.com>
2025-01-07 06:34:36 +0200

    hotfix add Readme

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -9,7 +9,9 @@ interface PageProps {
 
 export default function Page({ children, className, breadcrumbs = true }: PropsWithChildren<PageProps>) {
   return (
-    <main className={clsx('container max-w-2xl lg:max-w-7xl p-5 my-0 mx-auto min-h-[calc(100vh_-_72px)]', className)}>
+    <main
+      className={clsx('container max-w-2xl lg:max-w-7xl px-5 py-10 my-0 mx-auto min-h-[calc(100vh_-_72px)]', className)}
+    >
       {breadcrumbs ? <Breadcrumbs /> : null}
       {children}
     </main>
