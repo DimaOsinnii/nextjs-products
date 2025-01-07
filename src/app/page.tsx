b6beccfdfc2061ebf0da6e5f4164cfcd776568c9
autumn_dima <ozerland@gmail.com>
2025-01-07 05:09:50 +0200

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
 import Page from '@/components/page';
+import Link from 'next/link';
+
+export const metadata = {
+  title: 'Welcome to TechCart',
+};
 
 export default function Home() {
-  return <Page></Page>;
+  return (
+    <Page breadcrumbs={false} className="flex justify-center items-center">
+      <div className="flex z-10 relative flex-col items-center text-center gap-10">
+        <h1 className="text-4xl md:text-6xl lg:text-8xl">
+          Welcome to t<span className="text-green-400">e</span>ch <br /> <span className="text-green-400">e</span>
+          -commerce
+        </h1>
+        <Link
+          href="/products"
+          className="rounded-md bg-green-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:green-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-400"
+        >
+          Go to catalog
+        </Link>
+      </div>
+      <div className="absolute z-0 inset-0 aspect-[1.7] w-full h-full bg-gradient-to-r from-green-100 to-white/10 animate-morph" />
+    </Page>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      extend: {
+        animation: {
+          morph: 'morph 3s infinite ease-in-out', // Custom animation
+        },
+        keyframes: {
+          morph: {
+            '0%': {
+              'clip-path':
+                'polygon(46% 42%, 17% 70%, 122% 12%, 57% 75%, 86% 27%, -1% 71%, 14% 20%, 25% 128%, 44% 10%, 92% 95%, 31% 21%, 51% 94%, -14% 35%, 29% 66%, 41% 86%, 25% -3%)',
+            },
+            '50%': {
+              'clip-path':
+                'polygon(42% 39%, 20% 67%, 118% 14%, 54% 78%, 89% 30%, 3% 68%, 10% 23%, 28% 125%, 40% 13%, 95% 92%, 28% 24%, 53% 91%, -11% 37%, 26% 63%, 43% 83%, 23% 0%)',
+            },
+            '100%': {
+              'clip-path':
+                'polygon(46% 42%, 17% 70%, 122% 12%, 57% 75%, 86% 27%, -1% 71%, 14% 20%, 25% 128%, 44% 10%, 92% 95%, 31% 21%, 51% 94%, -14% 35%, 29% 66%, 41% 86%, 25% -3%)',
+            },
+          },
+        },
       },
     },
   },
